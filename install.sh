5b07e59794a2a3575ba40139abe5e876deda0258
王涵 <daoshuti@163.com>
2017-05-03 15:33:03 +0800

    给 install.sh 增加提示信息

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 # File Name: install.sh
 # Description:
 
+echo "Install boot2miui ..."
+
 echo "#boot2miui" >> ~/.bashrc
 echo "alias boot2miui='`pwd`/mybootimage2miui.sh'" >> ~/.bashrc 
 
+echo "done."
