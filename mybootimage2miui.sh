2804f8e7c0ad7b561f3a24424e65e0b0c341433f
wanghan <daoshuti@163.com>
2017-05-03 15:25:26 +0800

    modify mybootimage2miui.sh

--- mybootimage2miui.sh
+++ mybootimage2miui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Author: wanghan
-# Created Time : 2017年05月 3日 11:45:32
+# Created Time : Fri 28 Apr 2017 04:04:09 PM CST
 # File Name: mybootimage2miui.sh
 # Description:
 
@@ -14,8 +14,7 @@ NEWBOOT_OUTPUT_PATH=.
 function help()
 {
 	echo "Usage:"
-	echo "      mybootimage2miui.sh [my boot.img] [miui boot.img]
-	[output_path]"
+	echo "      mybootimage2miui.sh [my boot.img] [miui boot.img] [output_path]"
 }
 
 #检查是否有boot.img
@@ -27,80 +26,50 @@ function check_boot()
 	then
 		echo "${MYBOOT_PATH}/boot.img exist."
 	else
-		echo
-		"${MYBOOT_PATH}/boot.img
-		not exist!"
+		echo "${MYBOOT_PATH}/boot.img not exist!"
 		exit -1
 	fi
 
-	if test
-		-e
-		${MIUIBOOT_PATH}/boot.img 
+	if test -e ${MIUIBOOT_PATH}/boot.img 
 	then
-		echo
-		"${MIUIBOOT_PATH}/boot.img
-		exist."
+		echo "${MIUIBOOT_PATH}/boot.img exist."
 	else
-		echo
-		"${MIUIBOOT_PATH}/boot.img
-		not
-		exist!"
-		exit
-		-1
+		echo "${MIUIBOOT_PATH}/boot.img not exist!"
+		exit -1
 	fi
 }
 
 #解包
-function
-unboot_img()
+function unboot_img()
 {
-	#解包miui
-	boot.img
-	${BOOTTOOL_PATH}/mkboot
-	${MIUIBOOT_PATH}/boot.img
-	${MIUIBOOT_PATH}/output_miui
-	#解包我的
-	boot.img
-	${BOOTTOOL_PATH}/mkboot
-	${MYBOOT_PATH}/boot.img
-	${MYBOOT_PATH}/output_my
+	#解包miui boot.img
+	${BOOTTOOL_PATH}/mkboot ${MIUIBOOT_PATH}/boot.img ${MIUIBOOT_PATH}/output_miui
+	#解包我的 boot.img
+	${BOOTTOOL_PATH}/mkboot ${MYBOOT_PATH}/boot.img ${MYBOOT_PATH}/output_my
 }
 
 #替换
-function
-replace_kernel()
+function replace_kernel()
 {
-	rm
-	${MIUIBOOT_PATH}/output_miui/kernel
-	cp
-	${MYBOOT_PATH}/output_my/kernel
-	${MIUIBOOT_PATH}/output_miui
+	rm ${MIUIBOOT_PATH}/output_miui/kernel
+	cp ${MYBOOT_PATH}/output_my/kernel ${MIUIBOOT_PATH}/output_miui
 }
 
 #打包
-function
-boot_img()
+function boot_img()
 {
-	${BOOTTOOL_PATH}/mkboot
-	${MIUIBOOT_PATH}/output_miui
-	${NEWBOOT_OUTPUT_PATH}/boot.img
+	${BOOTTOOL_PATH}/mkboot ${MIUIBOOT_PATH}/output_miui ${NEWBOOT_OUTPUT_PATH}/boot.img
 }
 
 #清空文件
-function
-clean()
+function clean()
 {
-	rm
-	-rf
-	${MYBOOT_PATH}
-	rm
-	-rf
-	${MIUIBOOT_PATH}
+	rm -rf ${MYBOOT_PATH}
+	rm -rf ${MIUIBOOT_PATH}
 }
 
 #主函数
-function
-main()
+function main()
 {
 
 	#检查是否有boot.img
@@ -114,66 +83,30 @@ main()
 	#清空文件
 	clean
 
-	echo
-	"done."
+	echo "done."
 }
 
 #RUN
-echo
-"\$#
-is
-$#"
-if
-	[
-	$#
-	-lt
-	2
-	-o
-	$#
-	-gt
-	3
-	]
+echo "\$# is $#"
+if [ $# -lt 2 -o $# -gt 3 ]
 then
 	help
-	exit
-	-1
+	exit -1
 fi
-mkdir
--p
-${MYBOOT_PATH}
-mkdir
--p
-${MIUIBOOT_PATH}
-cp
-$1
-${MYBOOT_PATH}
-echo
-"MYBOOT_PATH
-is
-${MYBOOT_PATH}"
-cp
-$2
-${MIUIBOOT_PATH}
-echo
-"MIUIBOOT_PATH
-is
-${MIUIBOOT_PATH}"
-if
-	[
-	!
-	$3
-	]
+mkdir -p ${MYBOOT_PATH}
+mkdir -p ${MIUIBOOT_PATH}
+cp $1 ${MYBOOT_PATH}
+echo "MYBOOT_PATH is ${MYBOOT_PATH}"
+cp $2 ${MIUIBOOT_PATH}
+echo "MIUIBOOT_PATH is ${MIUIBOOT_PATH}"
+if [ ! $3 ]
 then
 	NEWBOOT_OUTPUT_PATH=.
-	echo
-	"NEWBOOT_OUTPUT_PATH
-	is
-	`pwd`"
+	echo "NEWBOOT_OUTPUT_PATH is `pwd`"
 else
 	NEWBOOT_OUTPUT_PATH=$3
-	echo
-	"NEWBOOT_OUTPUT_PATH
-	is
-	$3"
+	echo "NEWBOOT_OUTPUT_PATH is $3"
 fi
 main
+
+
